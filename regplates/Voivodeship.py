81b8dad8acd8f81b62c51e7a02736e32ec83bc5b
Maks1232 <maksymilian.karasek@o2.pl>
2024-01-25 23:43:56 +0100

    V20.0 preparation for the PyPI repo

---
+++ regplates/Voivodeship.py
@@ -0,0 +1,155 @@
+import random
+import pickle
+import pandas as pd
+
+
+def import_list(file_name):
+    with open(file_name, 'rb') as file:
+        imported_list = pickle.load(file)
+    return imported_list
+
+
+class Voivodeship:
+    """
+    A class used to get random registration plate index, correct city name, and four element list with obtained answers
+
+    Attributes
+    ----------
+    voivodeship : str
+        an attribute that determines range of available registration plates to be drawn
+    level : str
+        an attribute that determines difficulty level
+    mode : int
+        an attribute that determines game mode repetitive or non-repetitive
+    already_selected : dictionary
+        a dictionary with already used keys and values in particular object instance
+    voivodeship_options : list
+        a list of strings representing the available voivodeship options
+    loaded_dicts : list
+        a list of dictionaries including set of all indices and city names as keys and values
+    merged_dicts: dictionary
+        a dictionary including all dictionaries from loaded dictionaries merged in one
+    levenshtein_matrix: DataFrame
+        a matrix including Levenshtein distances between city names
+    extreme_matrix: DataFrame
+        a matrix including own metric similarity values
+    all: int
+        a value storing number of all available registration plates due to current parameter configuration
+
+    Methods
+    -------
+    random_plate()
+        a method being rule set for registration plate index selection
+    dictionary_merge()
+        a method which defines merged_dicts attribute content
+    get_random_question()
+        a method which returns registration plate index with repetitions or no repetitions drawn from data set in which
+        content is conditioned by parameters provided in __init__ method arguments
+    generate_answers(correct_answer)
+        a method which returns four element, shuffled list with answers without repetitions drawn from data set in which
+        content is conditioned by parameters provided in __init__ method arguments
+    ask_question()
+        a method which returns registration plate, belonged city name and four element list with th drawn answers
+    """
+    def __init__(self, voivodeship, level, mode=1):
+        self.voivodeship = voivodeship
+        self.level = level
+        self.mode = mode
+        self.already_selected = {}
+        self.voivodeship_options = import_list('voivodeship_options')
+        self.loaded_dicts = import_list('dicts.pickle3')
+        self.merged_dicts = self.dictionary_merge()
+        self.levenshtein_matrix = pd.read_pickle('levenshtein_matrix.pickle')
+        self.extreme_matrix = pd.read_pickle('extreme_matrix.pickle')
+        if not self.voivodeship == self.voivodeship_options[-1]:
+            self.all = len(self.loaded_dicts[self.voivodeship_options.index(self.voivodeship)])
+        else:
+            self.all = 409
+
+    def random_plate(self):
+
+        if self.voivodeship == self.voivodeship_options[-1]:
+            if self.mode == 1:
+                plate, county = random.choice(list(self.merged_dicts.items() - self.already_selected.items()))
+            else:
+                plate, county = random.choice(list(self.merged_dicts.items()))
+
+        elif self.level == "Hard":
+            if self.mode == 1:
+                plate, county = random.choice(
+                    list(self.loaded_dicts[self.voivodeship_options.index(self.voivodeship)].items() -
+                         self.already_selected.items()))
+            else:
+                plate, county = random.choice(list(
+                    self.loaded_dicts[self.voivodeship_options.index(self.voivodeship)].items()))
+        else:
+            if self.mode == 1:
+                plate, county = random.choice(list(self.loaded_dicts[self.voivodeship_options.index(
+                    self.voivodeship)].items() - self.already_selected.items()))
+            else:
+                plate, county = random.choice(list(self.loaded_dicts[self.voivodeship_options.index(
+                    self.voivodeship)].items()))
+
+        return plate, county
+
+    def dictionary_merge(self):
+        merged = {}
+        for _dict in self.loaded_dicts:
+            merged.update(_dict)
+        return merged
+
+    def get_random_question(self):
+
+        if self.voivodeship == self.voivodeship_options[-1]:
+            self.dictionary_merge()
+            plate, county = self.random_plate()
+        else:
+            plate, county = self.random_plate()
+
+        if self.mode == 1:
+            self.already_selected[plate] = county
+
+        return plate, county
+
+    def generate_answers(self, correct_answer):
+
+        answers = [correct_answer]
+
+        if self.level == "Hard":
+            col = self.levenshtein_matrix[correct_answer]
+            if isinstance(col, pd.Series):
+                col = col.to_frame()
+            col = col.sort_values(correct_answer)
+        elif self.level == "Extreme":
+            col = self.extreme_matrix[correct_answer]
+            if isinstance(col, pd.Series):
+                col = col.to_frame()
+            col = col.sort_values(correct_answer, ascending=False)
+        else:
+            col = None
+
+        iterator = 0
+
+        while len(answers) < 4:
+            if self.voivodeship == self.voivodeship_options[-1] and self.level == "Medium":
+                random_answer = random.choice(list(self.merged_dicts.values()))
+            elif self.level == "Hard" or self.level == 'Extreme':
+                random_answer = col.index[iterator]
+                iterator += 1
+            else:
+                random_answer = random.choice(
+                    list(self.loaded_dicts[self.voivodeship_options.index(self.voivodeship)].values()))
+
+            if random_answer not in answers:
+                answers.append(random_answer)
+
+        random.shuffle(answers)
+
+        return answers
+
+    def ask_question(self):
+
+        question, correct_answer = self.get_random_question()
+        answers = self.generate_answers(correct_answer)
+
+        return question, correct_answer, answers
---
+++ regplates/main.py
@@ -0,0 +1,255 @@
+from itertools import islice
+import pygame.freetype
+from pygame.locals import *
+from .config import *
+
+"""
+Configuration file with pygame initialization, color definitions, image imports and variable set
+
+Functions
+----------
+load_df(file)
+    a function used to read excel file and put it into DataFrame collection sorted by last column (score points) 
+    descending
+position_update()
+    a function used to make start window elements resizable
+draw_rect(position, text, button_color)
+    a function used to draw rectangular with frame and filled out with the text in specific position
+write_text(text, location)
+    a function used to write and blit the text in specific position
+draw_list(position, options, active_list, active_choice)
+    a function used to draw dropdown list in specific position
+render_dataframe(df)
+    a function which render DataFrame collection content to be displayed in GUI
+nick_input()
+    a function used to initialization elements needed for getting user name function control   
+get_user_name()
+    a function used to manage getting user name view
+handle_drop_down_event(_drop_down, _options, _event, _full_list_flag, _active_option)
+    a function used to spot and execute dropdown list handling
+"""
+
+
+def load_df(file):
+    df = pd.read_excel(file)
+    df.sort_values(by=df.columns[-1], ascending=False, inplace=True)
+    return df
+
+
+def position_update():
+    _drop_down = pygame.Rect(0.1 * WIN.get_width(),
+                             20 + 1.1 * app_logo.get_height(),
+                             0.25 * WIN.get_width(),
+                             0.04 * WIN.get_height())
+
+    _level_drop_down = pygame.Rect(0.9 * WIN.get_width() - _drop_down[2],
+                                   20 + 1.1 * app_logo.get_height(),
+                                   0.25 * WIN.get_width(),
+                                   0.04 * WIN.get_height())
+
+    _play_button = pygame.Rect(0.65 * WIN.get_width(),
+                               0.82 * WIN.get_height(),
+                               0.25 * WIN.get_height(),
+                               0.04 * WIN.get_height())
+
+    _exit_button = pygame.Rect(0.65 * WIN.get_width(),
+                               0.86 * WIN.get_height(),
+                               0.25 * WIN.get_height(),
+                               0.04 * WIN.get_height())
+
+    _mode_button = pygame.Rect(WIN.get_width() / 2 - 0.0625 * WIN.get_width(),
+                               20 + 1.1 * app_logo.get_height(),
+                               0.125 * WIN.get_width(),
+                               0.04 * WIN.get_height())
+
+    return _drop_down, _level_drop_down, _play_button, _exit_button, _mode_button
+
+
+def draw_rect(position, text, button_color):
+    pygame.draw.rect(WIN, button_color, position)  # draw rectangular
+    pygame.draw.rect(WIN, white, position, 2)  # draw the frame
+    write_text(text, (position[0] + position[2] // 2, position[1] + position[3] // 2))
+
+
+def write_text(text, location):
+    font = pygame.font.Font(None, 32)
+    text_surface = font.render(text, True, white)
+    text_location = text_surface.get_rect(center=location)
+    WIN.blit(text_surface, text_location)
+
+
+def draw_list(position, options, active_list, active_choice):
+    draw_rect(position, active_choice, active_green if active_list else green)
+
+    if active_list:
+        for i, option in enumerate(options):
+            y = position[1] + (i + 1) * position.height
+
+            pygame.draw.rect(WIN, dark_grey, (position[0], y, position.width, position.height))
+
+            pygame.draw.rect(WIN, white, (position[0], y, position.width, position.height), 2)
+            write_text(option, (position[0] + position.width // 2, y + position.height // 2))
+
+
+def render_dataframe(df):
+
+    font = pygame.font.Font(None, 32)
+    font_2 = pygame.font.Font(None, 72)
+
+    _x, _y = WIN.get_width() / 10, WIN.get_height() / 2.8
+    cell_width, cell_height = WIN.get_width() / 5, WIN.get_height() / 30
+
+    text = font_2.render("Ranking:", True, dark_blue)
+    WIN.blit(text, (20, 20 + WIN.get_height() / 3.9))
+
+    for col in df.columns[1:]:
+        text = font.render(col, True, black)
+        WIN.blit(text, (_x - WIN.get_width() / 300, 20 + _y))
+        _x += cell_width
+
+    _y += 2 * cell_height
+
+    for num, (_, row) in enumerate(islice(df.iterrows(), 10), start=1):
+        _x = 20
+        for value in row:
+            if isinstance(value, int) and value is not row.iloc[-1]:
+                text = font.render(str(num), True, black)
+            else:
+                text = font.render(str(value), True, black)
+
+            if _x == 20:
+                WIN.blit(text, (_x, _y))
+            else:
+                WIN.blit(text, (_x - WIN.get_width() / 9, _y))
+            _x += cell_width
+        _y += cell_height
+
+
+def nick_input():
+    _manager = pygame_gui.UIManager((WIN.get_width(), WIN.get_height()))
+    _frame = (WIN.get_width() / 2, WIN.get_height() / 20)
+    _text_input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect(
+        (WIN.get_width() / 2 - _frame[0] / 2, WIN.get_height() / 2 - _frame[1] / 2), _frame),
+        manager=_manager, object_id='#main_text_entry')
+
+    return _manager, _frame, _text_input
+
+
+def get_user_name():
+
+    font = pygame.font.Font(None, 32)
+    text = font.render("Wprowadź nazwę użytkownika i wciśnij klawisz ENTER:", True, dark_blue)
+    _run = True
+
+    manager, frame, text_input = nick_input()
+    text_input.focus()
+
+    while _run:
+
+        for _event in pygame.event.get():
+            if _event.type == VIDEORESIZE:
+                manager, frame, text_input = nick_input()
+                text_input.focus()
+            elif _event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and _event.ui_object_id == '#main_text_entry':
+                _run = False
+                return _event.text
+
+            manager.process_events(_event)
+
+        manager.update(clock.tick(60) / 10000)
+
+        WIN.fill("white")
+        WIN.blit(text, (WIN.get_width() / 2 - text.get_width() / 2,
+                        text_input.get_starting_height() + 5 * text.get_height()))
+
+        manager.draw_ui(WIN)
+        pygame.display.update()
+
+
+def handle_drop_down_event(_drop_down, _options, _event, _full_list_flag, _active_option, event):
+    if _full_list_flag:
+        for _i, _option in enumerate(_options):
+            _y = _drop_down.y + (_i + 1) * _drop_down.height
+            square_option = pygame.Rect(_drop_down.x, _y, _drop_down.width, _drop_down.height)
+
+            if square_option.collidepoint(event.pos):
+                _active_option = _option
+                _full_list_flag = False
+    return _active_option, _full_list_flag
+
+
+def main_function():
+    # PyGame initialization
+    pygame.init()
+    pygame.font.init()
+    pygame.mixer.init()
+
+    # Window creation
+    WIN = pygame.display.set_mode((1400, 800), pygame.RESIZABLE)
+    pygame.display.set_caption("Registration Plates Quiz")
+
+    run = True
+    full_list = False
+    nick_executed = False
+    full_level_list = False
+    nickname = "Unknown"
+    active_option = "Wybierz województwo"
+    active_level_option = "Wybierz poziom"
+    tribes = {0: 'Powtarzanie', 1: 'Bez powtórzeń'}
+    mode = 0
+
+    while run:
+
+        drop_down, level_drop_down, play_button, exit_button, mode_button = position_update()
+        WIN.fill(white)
+        WIN.blit(app_logo, (0.5 * WIN.get_width() - 0.5 * app_logo.get_width(), 0.02 * WIN.get_height()))
+
+        render_dataframe(load_df("ranking.xlsx"))
+
+        for event in pygame.event.get():
+            if not nick_executed:
+                nickname = get_user_name()
+                nick_executed = True
+            if event.type == pygame.QUIT:
+                run = Game.exit_execute()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    run = Game.exit_execute()
+            elif event.type == VIDEORESIZE:
+                WIN = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if drop_down.collidepoint(event.pos):
+                    full_list = not full_list
+                    full_level_list = False
+                elif exit_button.collidepoint(event.pos):
+                    run = Game.exit_execute()
+                elif level_drop_down.collidepoint(event.pos):
+                    full_level_list = not full_level_list
+                    full_list = False
+                elif mode_button.collidepoint(event.pos):
+                    mode = not mode
+                elif full_list:
+                    active_option, full_list = handle_drop_down_event(drop_down, voivodeship_options, event, full_list,
+                                                                      active_option, event)
+                elif full_level_list:
+                    active_level_option, full_level_list = handle_drop_down_event(level_drop_down, level_options, event,
+                                                                                  full_level_list, active_level_option, event)
+                if play_button.collidepoint(event.pos):
+                    if not active_option == "Wybierz województwo" and not active_level_option == "Wybierz poziom":
+                        if active_option == voivodeship_options[-1] and active_level_option == level_options[0]:
+                            Game.config_notification()
+                        else:
+                            game_instance = Game(WIN, active_option, active_level_option, mode, nickname)
+
+        draw_rect(mode_button, tribes[mode], dark_grey if mode != 0 else grey)
+        draw_list(drop_down, voivodeship_options, active_list=full_list, active_choice=active_option)
+        draw_list(level_drop_down, level_options, active_list=full_level_list, active_choice=active_level_option)
+        draw_rect(exit_button, "Wyjdź", grey)
+        draw_rect(play_button, "Zagraj!",
+                  green if active_option != 'Wybierz województwo' and active_level_option != 'Wybierz poziom' else grey)
+
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main_function()
